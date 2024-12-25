64d1ebbc8b5e4a40f3a434a48263a1b8a73cfbdc
Jeyabal <jeyabal1993@gmail.com>
2024-12-24 22:18:44 -0500

    Adding Hero Text - Intro

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,7 +3,13 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className='hero'>
+    <div className='hero container'>
+      <div className='hero-text'>
+        <h1>Welcome to Dungeons & Dragons!</h1>
+        <p>Enter the world of unlimited fun with friends and families. 
+          Over a millions people play D&D every month.</p>
+        <button className='btn'>Get Started</button>
+      </div>
     </div>
   )
 }
