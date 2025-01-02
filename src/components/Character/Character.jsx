020bdefb214279895447666d5aade11a3f940f46
Jeyabal <jeyabal1993@gmail.com>
2025-01-01 22:54:42 -0500

    adding character page

---
+++ src/components/Character/Character.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Character.css'
+
+const Character = () => {
+  return (
+    <div className='character'>
+        <div className="charcter-bio"></div>
+        <div className="character-stats"></div>
+            <h2>Sample stats</h2>
+            <h3>Strength 18</h3>
+            <h3>Consititution 15</h3>
+            <h3>Wisdom 20</h3>
+    </div>
+  )
+}
+
+export default Character
