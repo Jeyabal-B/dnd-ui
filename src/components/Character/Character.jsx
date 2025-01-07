6fa059187bfac961c7fe547db2a9a7ca953dcd54
Jeyabal <jeyabal1993@gmail.com>
2025-01-06 23:37:58 -0500

    Adding sample get API and logging it on the console

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -2,14 +2,26 @@ import React from 'react'
 import './Character.css'
 
 const Character = () => {
+
+  const allCharacters = async () => {
+
+    const URL = 'https://catfact.ninja/fact';
+    const response = await fetch(URL);
+    const body = await response.json;
+    console.log(response);
+  }
+
   return (
     <div className='character'>
         <div className="charcter-bio"></div>
         <div className="character-stats"></div>
             <h2>Sample stats</h2>
-            <h3>ADD FETCH API CALL HERE, integrate other services</h3>
+            <button onClick={allCharacters}>
+              Get All
+            </button>
     </div>
   )
+
 }
 
 export default Character
