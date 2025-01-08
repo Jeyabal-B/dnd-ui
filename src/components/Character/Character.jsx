703ac660f7ecc1e76e915e0eb75b8652477274fc
Jeyabal <jeyabal1993@gmail.com>
2025-01-08 00:32:26 -0500

    get all characters works and displays in UI

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -10,35 +10,35 @@ const Character = () => {
     const URL = '/characters/getAll';
     const response = await fetch(URL);
     const body = await response.json();
-    
-    console.log('Response Body: ',body);
+
     setCharacters(body);
-    console.log('Response Body: after parse ',characters);
+    console.log('Response Body: ', characters);
   }
 
   return (
-    <div className='character'>
-        <div className="charcter-bio"></div>
-        <div className="character-stats"></div>
-            <h2>Sample stats</h2>
-            <button onClick={allCharacters}>
-              Get All
-            </button>
-            {
-              characters.length > 0 && (
-                <ul>
-                  {characters.map((character, index) => (
-                    <li key={index}>
-                      {character.Name}
-                    </li>
-                  )
-                  )}
-                </ul>
-              ) 
-            }
+    <div className='character-container'>
+      <button onClick={allCharacters}>
+        Get All
+      </button>
+      <div className='character'>
+          {characters.map((char) => (
+            <div key={char._id}>
+              <div className="character-card">
+                <h2>{char.name}</h2>
+                <p>Class : {char.class}</p>
+              </div>
+              <div className="character-stats">
+                <p>Race :{char.race}</p>
+                <p>Gender :{char.gender}</p>
+                <p>Feats :{char.feats}</p>
+                <p>Spell Slots :{char.spellSlots}</p>
+                <p>Background :{char.backgroundInfo}</p>
+              </div>
+            </div>
+          ))}
+        </div>
     </div>
   )
-
 }
 
 export default Character
