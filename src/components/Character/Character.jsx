b16c3ef9606d476ac9e830704bc97c57fe2cf7fa
Jeyabal <jeyabal1993@gmail.com>
2025-01-14 00:49:20 -0500

    Introducing add new character api

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -15,12 +15,19 @@ const Character = () => {
     console.log('Response Body: ', characters);
   }
 
+  const newCharacter = async () => {
+    const URL = '/characters/add';
+
+    console.log('Request Body: ', characters);
+  }
+
   return (
     <div className='character-container'>
-      <button onClick={allCharacters}>
-        Get All
-      </button>
-      <div className='character'>
+      <div className='character-get-all'>
+        <button onClick={allCharacters}>
+          Get All
+        </button>
+        <div className='character'>
           {characters.map((char) => (
             <div key={char._id}>
               <div className="character-card">
@@ -37,6 +44,29 @@ const Character = () => {
             </div>
           ))}
         </div>
+      </div>
+      <div className='character-add-new'>
+        <button onClick={newCharacter}>
+          Create New
+        </button>
+        <div className='character-new'>
+          {characters.map((char) => (
+            <div key={char._id}>
+              <div className="character-card">
+                <h2>{char.name}</h2>
+                <p>Class : {char.class}</p>
+              </div>
+              <div className="character-stats">
+                <p>Race :{char.race}</p>
+                <p>Gender :{char.gender}</p>
+                <p>Feats :{char.feats}</p>
+                <p>Spell Slots :{char.spellSlots}</p>
+                <p>Background :{char.backgroundInfo}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
