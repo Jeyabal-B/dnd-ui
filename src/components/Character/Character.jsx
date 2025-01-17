9359bdb79bdf13f0a908df535c30c44cfb7aa97a
Jeyabal <jeyabal1993@gmail.com>
2025-01-16 23:56:21 -0500

    add new option displays conditional content on click

--- src/components/Character/Character.jsx
+++ src/components/Character/Character.jsx
@@ -3,71 +3,75 @@ import './Character.css'
 
 const Character = () => {
 
+  const [currentScreen, setCurrentScreen] = useState('character-container');
   const [characters, setCharacters] = useState([]);
+  const [character, createCharacter] = useState([]);
 
   const allCharacters = async () => {
 
     const URL = '/characters/getAll';
     const response = await fetch(URL);
     const body = await response.json();
-
     setCharacters(body);
     console.log('Response Body: ', characters);
   }
 
   const newCharacter = async () => {
     const URL = '/characters/add';
+    const response = await fetch(URL);
+    const body = await response.json();
+    createCharacter(body);
+    console.log('Request Body: ', character);
+  }
 
-    console.log('Request Body: ', characters);
+  const showCreateOptions = () => {
+    setCurrentScreen('create-character');
   }
 
   return (
-    <div className='character-container'>
-      <div className='character-get-all'>
-        <button onClick={allCharacters}>
-          Get All
-        </button>
-        <div className='character'>
-          {characters.map((char) => (
-            <div key={char._id}>
-              <div className="character-card">
-                <h2>{char.name}</h2>
-                <p>Class : {char.class}</p>
+    
+    
+      <div className='character-container'>
+        <div className='character-get-all'>
+
+          <button onClick={allCharacters}>
+            Get All
+          </button>
+
+          <div className='character'>
+            {characters.map((char) => (
+              <div key={char._id}>
+                <div className="character-card">
+                  <h2>{char.name}</h2>
+                  <p>Class : {char.class}</p>
+                </div>
+                <div className="character-stats">
+                  <p>Race :{char.race}</p>
+                  <p>Gender :{char.gender}</p>
+                  <p>Feats :{char.feats}</p>
+                  <p>Spell Slots :{char.spellSlots}</p>
+                  <p>Background :{char.backgroundInfo}</p>
+                </div>
               </div>
-              <div className="character-stats">
-                <p>Race :{char.race}</p>
-                <p>Gender :{char.gender}</p>
-                <p>Feats :{char.feats}</p>
-                <p>Spell Slots :{char.spellSlots}</p>
-                <p>Background :{char.backgroundInfo}</p>
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-      <div className='character-add-new'>
-        <button onClick={newCharacter}>
-          Create New
-        </button>
-        <div className='character-new'>
-          {characters.map((char) => (
-            <div key={char._id}>
-              <div className="character-card">
-                <h2>{char.name}</h2>
-                <p>Class : {char.class}</p>
-              </div>
-              <div className="character-stats">
-                <p>Race :{char.race}</p>
-                <p>Gender :{char.gender}</p>
-                <p>Feats :{char.feats}</p>
-                <p>Spell Slots :{char.spellSlots}</p>
-                <p>Background :{char.backgroundInfo}</p>
-              </div>
+
+        <div className='character-add-new'>
+
+          <button onClick={showCreateOptions}>
+            Create New
+          </button>
+          {currentScreen === 'create-character' && (
+            <div>
+              This is just a sample page to create a new character. Details TBD.
             </div>
-          ))}
+          )}
+          
         </div>
+
       </div>
-    </div>
+    
   )
 }
 
